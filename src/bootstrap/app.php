5a9aa3050e4b71a87ffd861ee62b2c62bae8abb2
NakatsutsumiChikana-NIXE <c-nakatsutsumi@nixe-inc.com>
2024-10-22 17:32:35 +0900

    api.phpの導入と設定

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -7,11 +7,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        apiPrefix: '', // これを記載することによってapi.phpに記載したrouteのリンクが、/api/がなくても動作させることができます
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // ここでミドルウェアを登録します
+        // $middleware->push(\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
+        // $middleware->push('throttle:api');
+        // $middleware->push(\Illuminate\Routing\Middleware\SubstituteBindings::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
